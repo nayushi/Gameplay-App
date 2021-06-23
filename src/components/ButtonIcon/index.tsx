9ac69ab72fe292fbb649a2844ae6b87c032bdf11
Mariana Brasil <nayushi@qq.com>
2021-06-23 11:22:43 -0300

    Button icon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { Text, Image, View } from "react-native";
+import discord from "../../assets/discord.png";
+import { styles } from "./styles";
+
+type Props =  RectButtonProps & {
+  title: string;
+};
+
+export function ButtonIcon({ title , ...rest} : Props) {
+  return (
+    <RectButton style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={discord} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </RectButton>
+  );
+}
